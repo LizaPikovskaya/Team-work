490be15407705eed68adb4d9a505082d7f488f85
LizaPikovskaya <117260865+LizaPikovskaya@users.noreply.github.com>
2023-04-15 14:00:42 +0000

    Deploying to gh-pages from @ LizaPikovskaya/Team-work@bc49d6626841cdc5592a13968bc1da46e4f9483d 🚀

--- index.08b52b3c.js
+++
@@ -1,2 +0,0 @@
-var t={form:document.querySelector("#task-form"),list:document.querySelector("#task-list")};var e={addNote:function(t){const e=JSON.parse(localStorage.getItem("notes-data"))||[];e.push(t),localStorage.setItem("notes-data",JSON.stringify(e))}};function o(t){const e={id:Date.now()};return new FormData(t.currentTarget).forEach(((t,o)=>{e[o]=t})),e}t.form.addEventListener("submit",(function(t){t.preventDefault(),e.addNote(o(t))}));
-//# sourceMappingURL=index.08b52b3c.js.map
